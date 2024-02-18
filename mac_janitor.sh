02e273832a44a2797a2207d9c28bf24beb24817b
Darien <djentwistle@gmail.com>
2024-02-17 22:58:27 -0500

    Add files via upload

---
+++ mac_janitor.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+######################################################################################
+# This script does some maintenance operations on Mac OS X (10.11 and greater)      #
+#     v4.0 	Codename Wrasse /  2024 Build                                     #
+######################################################################################
+
+echo Mac Janitor by Darien Entwistle
+echo   v4.0 	build CarsNCoffee 2024 Build
+sleep 2s
+
+# Check macOS version
+macos_version=$(sw_vers -productVersion)
+major_version=$(echo $macos_version | awk -F. '{print $1}')
+minor_version=$(echo $macos_version | awk -F. '{print $2}')
+if [ $major_version -gt 10 ] || [ $minor_version -ge 11 ]; then
+    echo "Your Mac OS version is at least 10.11, App will continue";
+else
+    echo "You have an INCOMPATIBLE Mac OS version! App will quit";
+    sleep 3s
+    exit
+fi
+
+## START
+
+echo -n "Would you like to run Mac Janitor?? Otherwise 'N' for No to quit & cancel!! "
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+echo Continuing! ... Running Mac Janitor!
+sleep 3s
+else
+echo Canceled! EXITING NOW!!!
+sleep 3s
+exit
+fi
+
+# Defined colors
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+STOPCOLOR='\033[0m'
+
+echo ${GREEN} MAC OS MAINTENANCE SCRIPT by Darien Entwistle  ${STOPCOLOR}
+echo ${RED} Please close all applications before giving the administrator password! The script may ask for your password again during the maintenance process if one specific task takes more than 5 minutes to complete on your system. ${STOPCOLOR}
+
+# Ask for super user passwd before printing the "Starting...". It just looks nicer.
+sudo ls / > /dev/null
+
+echo ${BLUE} Starting the maintenance process. ${STOPCOLOR}
+
+# Syncs ntpd
+echo ${YELLOW} Syncing Date and Time ${STOPCOLOR}
+sudo sntp -sS time.apple.com
+echo ${GREEN} Done. ${STOPCOLOR}
+
+#Performs some OS optimization
+echo ${YELLOW} OS Optimization ${STOPCOLOR}
+sudo defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+sudo defaults write com.apple.Dock autohide-delay -float 0
+sudo defaults write com.apple.finder DisableAllAnimations -bool true
+#Make all animations faster that are used by Mission Control.
+defaults write com.apple.dock expose-animation-duration -float 0.1
+# Disable animations when you open an application from the Dock.
+defaults write com.apple.dock launchanim -bool false
+
+
+sleep 4s
+echo ${GREEN} OS Optimization done, continuing onwards! .....${STOPCOLOR}
+
+
+# Clears Quick look cache
+echo ${YELLOW} Clearing Quick Look cache... ${STOPCOLOR}
+rm -rf $TMPDIR/../C/com.apple.QuickLook.thumbnailcache
+echo ${GREEN} Quick Look Cache cleared!! ${STOPCOLOR}
+
+#Clear DNS Cache
+echo ${YELLOW} Clearing DNS cache... ${STOPCOLOR}
+sudo dscacheutil -flushcache && \
+sudo killall -HUP mDNSResponder
+echo ${GREEN} DNS Cache cleared!! ${STOPCOLOR}
+
+# Clears font cache
+echo ${YELLOW} Clearing font cache... ${STOPCOLOR}
+sudo atsutil databases -remove
+echo ${GREEN} Done. ${STOPCOLOR}
+
+# Clean log files
+echo ${YELLOW} Cleaning log files... ${STOPCOLOR}
+sudo rm -rf /var/log/*.log
+echo ${GREEN} Done. ${STOPCOLOR}
+
+echo ${YELLOW} Emptying trash... ${STOPCOLOR}
+# Force empty trash
+sudo rm -rf ~/.Trash/*
+echo ${GREEN} Done. ${STOPCOLOR}
+
+echo ${YELLOW} Running built in maintenance scripts... ${STOPCOLOR}
+# Run maintenance scripts
+sudo periodic daily weekly monthly
+echo ${GREEN} Done. ${STOPCOLOR}
+
+echo ${YELLOW} Verifying HDD Volume ... ${STOPCOLOR}
+# Verifies the whole Macintosh HDD volume
+sudo diskutil verifyVolume /
+echo ${GREEN} Done. ${STOPCOLOR}
+
+echo ${YELLOW} Repairing startup volume... ${STOPCOLOR}
+# Repairs startup volume
+sudo fsck -fy
+echo ${GREEN} Done. ${STOPCOLOR}
+
+# Clears RAM for better memory mangement
+echo ${YELLOW} Clearing RAM Cache..... ${STOPCOLOR}
+sudo purge
+echo ${GREEN} Done. ${STOPCOLOR}
+
+# Clear Safari caches
+echo ${YELLOW} Clearing Safari caches... ${STOPCOLOR}
+rm -rf ~/Library/Caches/com.apple.Safari/*
+rm -rf ~/Library/Caches/Metadata/Safari/*
+echo ${GREEN} Safari caches cleared!! ${STOPCOLOR}
+
+# Clear system caches
+echo ${YELLOW} Clearing system caches... ${STOPCOLOR}
+sudo rm -rf /Library/Caches/*
+echo ${GREEN} System caches cleared!! ${STOPCOLOR}
+
+# Clear user caches
+echo ${YELLOW} Clearing user caches... ${STOPCOLOR}
+rm -rf ~/Library/Caches/*
+echo ${GREEN} User caches cleared!! ${STOPCOLOR}
+
+sleep 3s
+say "Mac Janitor has completed maintenance on your Mac"
+echo ${BLUE} Finished the whole maintenance process. ${STOPCOLOR}
+say System Needs to restart!
+echo Will will need to reboot. REBOOTING in 10 SECONDS!
+echo .....
+echo ..........
+echo  CTRL-C to Cancel!!
+echo  ...............
+sleep 10s
+sudo reboot
+fi
